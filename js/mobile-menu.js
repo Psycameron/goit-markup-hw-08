41d56b79fd437c926baca9582548adeb3f7e1fff
Dmitriy Bulakhov <bulakhovda@gmail.com>
2022-12-14 10:48:45 +0200

    Меню без анимации

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,21 +1,42 @@
+// (() => {
+//   const menuBtnRef = document.querySelector("[data-menu-button]");
+//   const mobileMenuRef = document.querySelector("[data-menu]");
+
+//   menuBtnRef.addEventListener("click", () => {
+//     const expanded =
+//       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    
+//     menuBtnRef.classList.toggle("is-open");
+
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
+//     mobileMenuRef.classList.toggle('is-open');
+
+//   });
+
+// })();
+
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-
   menuBtnRef.addEventListener("click", () => {
-    const expanded =
+    const isMenuOpen =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-    
     menuBtnRef.classList.toggle("is-open");
-
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
-    mobileMenuRef.classList.toggle('is-open');
-
+    menuBtnRef.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenuRef.classList.toggle("is-open");
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
   });
-
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenuRef.classList.remove('is-open');
+    menuBtnRef.classList.remove('is-open');
+    menuBtnRef.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
 })();
-
-
 
 
 
